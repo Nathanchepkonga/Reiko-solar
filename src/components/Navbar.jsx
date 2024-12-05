6b0ab995eb815e7a5119cc1b9ce8126a1bf06fe3
Nathanchepkonga <nathanchepkonga@gmail.com.com>
2024-12-05 03:27:08 +0300

    Nav Bar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,15 +7,17 @@ const Navbar = () => {
     <nav className="bg-green text-white py-4 px-6 flex items-center justify-between">
       {/* Logo */}
       <div className="flex items-center">
-        <img src={logo} alt="Reiko Solar Logo" className="h-10 w-auto mr-4" />
+        <Link to="/">
+          <img src={logo} alt="Reiko Solar Logo" className="h-10 w-auto mr-4" />
+        </Link>
         <h1 className="text-xl font-bold">Reiko Solar Kenya</h1>
       </div>
 
       {/* Navigation Links */}
       <div className="space-x-6">
         <Link to="/" className="hover:underline">Home</Link>
-        <Link to="/consultation" className="hover:underline">Consultation </Link>
-        <Link to="/installation" className="hover:underline">Installation </Link>
+        <Link to="/consultation" className="hover:underline">Consultation</Link>
+        <Link to="/installation" className="hover:underline">Installation</Link>
         <Link to="/about" className="hover:underline">About Us</Link>
       </div>
     </nav>
