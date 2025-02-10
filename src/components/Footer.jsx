73d897d1812bb5e322d16f33dbcda5e99310daab
Nathanchepkonga <nathanchepkonga@gmail.com.com>
2025-02-10 12:47:04 +0300

    twitter 2  commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -54,7 +54,7 @@ const Footer = () => {
               <FaFacebookF />
             </a>
             <a
-              href="https://www.twitter.com/PVsolutionsKE"
+              href="https://twitter.com/PVsolutionsKE"
               target="_blank"
               rel="noopener noreferrer"
               className="text-gray-400 hover:text-green-400 transition text-xl"
